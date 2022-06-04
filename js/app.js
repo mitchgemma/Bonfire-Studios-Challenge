0aee20c887243e18fd7e20d55e8c7f8e8cff0fdb
mitchgemma <mitchgemma@gmail.com>
2022-06-04 19:32:15 -0400

    show buttons fully functional - can likely use a refactor

--- js/app.js
+++ js/app.js
@@ -1,16 +1,35 @@
 //========DOM ELEMENTS======================================================================================
-
+const oddBoxes = document.querySelectorAll('.odd')
+const evenBoxes = document.querySelectorAll('.even')
 const showAll = document.querySelector('.show-all')
 const showOdd = document.querySelector('.show-even')
 const showEven = document.querySelector('.show-odd')
 
 const clickButton = (e) => {
-  console.log('the element', e.target)
   // if the target does not have an id, find the element with 'checked' id and remove it
   if (!e.target.id) {
     document.getElementById('checked').removeAttribute('id')
     // give the new element the id 'checked
     e.target.id = 'checked'
+  }
+  // if user clicks on show even button, displays will be changed accordingly
+  if (e.target.className == 'show-even') {
+    for (let i = 0; i < oddBoxes.length; i++) {
+      oddBoxes[i].style.display = 'none'
+      evenBoxes[i].style.display = 'flex'
+    }
+    // for show-odd button, even boxes will be hidden
+  } else if (e.target.className == 'show-odd') {
+    for (let i = 0; i < evenBoxes.length; i++) {
+      evenBoxes[i].style.display = 'none'
+      oddBoxes[i].style.display = 'flex'
+    }
+    // when show all is clicked, all displays will be changed back to normal
+  } else if (e.target.className == 'show-all') {
+    for (let i = 0; i < evenBoxes.length; i++) {
+      evenBoxes[i].style.display = 'flex'
+      oddBoxes[i].style.display = 'flex'
+    }
   }
 }
 
